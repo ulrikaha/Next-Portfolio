e05d1a9b2d480f349f2329ee4e36360c6dae0b6c
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2024-03-05 11:13:44 +0100

    Added submitBtn for the contact form

--- components/contact.tsx
+++ components/contact.tsx
@@ -2,11 +2,12 @@
 
 import React from 'react'
 import SectionHeading from './section-heading'
-import { FaPaperPlane } from "react-icons/fa";
 import { motion } from 'framer-motion';
 import { sendEmail } from '@/actions/sendEmail';
+import SubmitBtn from './submit-btn';
 
 export default function Contact() {
+   
 
     return (
         <motion.section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
@@ -50,14 +51,7 @@ export default function Contact() {
                     required
                     maxLength={500}
                 />
-                <button type="submit"
-                    className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-[#DB2777] text-white rounded-full outline-none transition-allfocus:scale-110 hover:scale-110 active:scale-105"
-                >
-                    Submit <FaPaperPlane className="text-xs 
-                opacity-70 transition-all
-                group-hover:translate-x-1
-                group-hover:-translate-y-1" /> {" "}
-                </button>
+               <SubmitBtn />
             </form>
         </motion.section>
     )
