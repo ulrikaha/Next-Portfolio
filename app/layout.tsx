e18501ce2ac2462f9f6739d3bd652cb04d056f36
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2024-03-12 10:34:01 +0100

    Added active-section-context

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Toaster } from "react-hot-toast";
+import  ActiveSectionContextProvider  from "@/context/active-section-context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,10 +25,12 @@ export default function RootLayout({
       >
         <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
         <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <ActiveSectionContextProvider>
         <Header />
         {children}
         <Footer />
         <Toaster position="top-right" />
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
