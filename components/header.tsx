d1cb2a20d87ab4d1c668d9f8d61ffaacc62ea55e
ulrikaha <98079099+ulrikaha@users.noreply.github.com>
2024-03-12 11:30:02 +0100

    Removed link and try use a tag instead

--- components/header.tsx
+++ components/header.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { links } from "@/lib/data";
-import Link from "next/link";
 import clsx from "clsx";
 import { useActiveSectionContext } from "@/context/active-section-context";
 
@@ -28,7 +27,7 @@ export default function Header() {
               initial={{ y: -100, opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
             >
-              <Link
+              <a
                 className={clsx(
                   "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
                   {
@@ -55,7 +54,7 @@ export default function Header() {
                     }}
                   ></motion.span>
                 )}
-              </Link>
+              </a>
             </motion.li>
           ))}
         </ul>
